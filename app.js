fa90e1fb394c5b49075873a84834cff77e31d879
Dries Hugaerts <dries.hugaerts@live.be>
2020-11-19 15:25:53 +0100

    created endpoints: - to provide source-data and form-data for a given application-form (uuid) - update an application-form for a given delta and application-form (uuid)

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 import { app, errorHandler } from 'mu';
 
-import { getFormData } from './lib/form-data';
+import bodyParser from 'body-parser';
+
+import { updateSourceData } from './lib/source-data';
+import { ApplicationForm } from './lib/application-form';
+import { getFileContent } from './lib/util/file';
+import { ACTIVE_FORM_URI } from './env';
+
+// TODO parser
+//  look up more in depth on how to use this
+
+app.use(bodyParser.json({
+  type: function(req) {
+    return /^application\/json/.test(req.get('content-type'));
+  },
+}));
 
 app.get('/', function(req, res) {
   const message = 'Hey there, you have reached subsidy-applications-management-service! Seems like I\'m doing just fine! :)';
@@ -9,10 +23,11 @@ app.get('/', function(req, res) {
 
 /**
  * Returns the **active** form-data in `text/turtle` format
+ * TODO change this endpoint to just return the URI off the active-form
  */
 app.get('/active-form-data', async function(req, res, next) {
   try {
-    const ttl = await getFormData();
+    const ttl = await getFileContent(ACTIVE_FORM_URI);
     return res.status(200).set('content-type', 'text/turtle').send(ttl);
   } catch (e) {
     console.log(`Something went wrong while retrieving the form-data:`);
@@ -21,6 +36,39 @@ app.get('/active-form-data', async function(req, res, next) {
   }
 });
 
+app.get('/application-forms/:uuid', async function(req, res, next) {
+  const uuid = req.params.uuid;
+  try {
+    const applicationForm = await new ApplicationForm().init(uuid);
+    const source = applicationForm.source;
+    const form = await getFileContent(applicationForm.formURI ? applicationForm.formURI : ACTIVE_FORM_URI);
+    return res.status(200).set('content-type', 'application/vnd.api+json').send({
+      form,
+      source,
+    });
+  } catch (e) {
+    if (e.status) {
+      return res.status(e.status).set('content-type', 'application/vnd.api+json').send({message: e.message});
+    }
+    console.log(`Something unexpected went wrong while retrieving the application-form with uuid <${uuid}>`);
+    console.log(e);
+    return next(e);
+  }
+});
 
+app.put('/application-forms/:uuid', async function(req, res, next) {
+  const uuid = req.params.uuid;
+  const delta = req.body;
+
+  try {
+    await new ApplicationForm().init(uuid); // NOTE check to ensure
+    await updateSourceData(delta);
+    return res.status(204).send();
+  } catch (e) {
+    console.log(`Something went wrong while updating source-data for application-form with uuid <${uuid}>`);
+    console.log(e);
+    return next(e);
+  }
+});
 
 app.use(errorHandler);
