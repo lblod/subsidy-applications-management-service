956cc408bcf1f6fd19ea3aeb545fab2fee0e9795
Dries Hugaerts <dries.hugaerts@live.be>
2020-12-01 13:12:56 +0100

    Removed to be deprecated env. variables.

--- env.js
+++ env.js
@@ -1,12 +1,8 @@
-// TODO to be deprecated
-const ACTIVE_FORM = process.env.ACTIVE_FORM || 'form.ttl';
 
 const META_DATA = process.env.META_DATA || 'meta.ttl';
 const FORM_DATA_DIR = process.env.FORM_DATA_DIR || '/share/form-versions/';
-const SERVICE_NAME = process.env.SERVICE_NAME || 'semantic-form-management-service';
+const SERVICE_NAME = process.env.SERVICE_NAME || 'subsidy-application-management-service';
 
-// TODO to be deprecated
-const ACTIVE_FORM_URI = `share://${ACTIVE_FORM}`;
 const META_DATA_URI = `share://${META_DATA}`;
 const APP_URI = `http://data.lblod.info/services/${SERVICE_NAME}`;
 
@@ -26,8 +22,6 @@ const DEFAULT_CONFIG = {
 const USER_CONFIG = require('/share/config.json') || DEFAULT_CONFIG;
 
 export {
-  ACTIVE_FORM,
-  ACTIVE_FORM_URI,
   META_DATA_URI,
   DEFAULT_CONFIG,
   USER_CONFIG,
