317eb7ef36689f72bae7ff2824a56be3f1dc1eff
Dries Hugaerts <dries.hugaerts@live.be>
2021-02-04 15:53:05 +0100

    bug-squash before alpha release.

--- env.js
+++ env.js
@@ -9,10 +9,10 @@ const SEMANTIC_FORM_TYPE = process.env.SEMANTIC_FORM_TYPE ||
 
 const MUTATION_QUERY_CHUNK_SIZE = process.env.MUTATION_QUERY_CHUNK_SIZE || 50;
 
-const VERSIONED_CONFIGURATION_ROOT = process.env.VERSIONED_CONFIGURATION_ROOT || '/config/versioned/';
+const VERSIONED_CONFIGURATION_ROOT = process.env.VERSIONED_CONFIGURATION_ROOT || '/config/versions/';
 const VERSIONED_CONFIGURATION_WATCHER = !!(process.env.VERSIONED_CONFIGURATION_WATCHER) || false;
 
-const META_DATA_ROOT = process.env.META_DATA_ROOT || '/data/meta/';
+const META_DATA_ROOT = process.env.META_DATA_ROOT || '/data/meta-files/';
 const META_DATA_CRON = process.env.META_DATA_CRON || undefined;
 
 const DEBUG_LOGS = !!(process.env.DEBUG_LOGS) || false;
