795fe975b8d90bbe9c49ed93b2caef4da31a5b1b
Dries Hugaerts <dries.hugaerts@live.be>
2021-02-04 12:18:23 +0100

    created configurable meta-data/file sync. cron-job + boy-scout.

--- env.js
+++ env.js
@@ -1,25 +1,26 @@
 
 const SERVICE_NAME = process.env.SERVICE_NAME || 'subsidy-application-management-service';
-const APP_URI = `http://data.lblod.info/services/${SERVICE_NAME}`;
-
+const SERVICE_URI = `http://data.lblod.info/services/${SERVICE_NAME}`;
 const SEMANTIC_FORM_TYPE = process.env.SEMANTIC_FORM_TYPE || 'http://lblod.data.gift/vocabularies/subsidie/ApplicationForm';
-const DATA_QUERY_CHUNK_SIZE = process.env.DATA_QUERY_CHUNK_SIZE || 50;
+
+const MUTATION_QUERY_CHUNK_SIZE = process.env.MUTATION_QUERY_CHUNK_SIZE || 50;
 
 const VERSIONED_CONFIGURATION_ROOT = process.env.VERSIONED_CONFIGURATION_ROOT || '/config/versioned/';
 const VERSIONED_CONFIGURATION_WATCHER = !!(process.env.VERSIONED_CONFIGURATION_WATCHER) || false;
 
 const META_DATA_ROOT = process.env.META_DATA_ROOT || '/data/meta/';
+const META_DATA_CRON = process.env.META_DATA_CRON || undefined;
 
 const DEBUG_LOGS = !!(process.env.DEBUG_LOGS) || false;
 
-
-
 export {
+  SERVICE_NAME,
+  SERVICE_URI,
   SEMANTIC_FORM_TYPE,
-  APP_URI,
-  DATA_QUERY_CHUNK_SIZE,
-  META_DATA_ROOT,
+  MUTATION_QUERY_CHUNK_SIZE,
   VERSIONED_CONFIGURATION_ROOT,
   VERSIONED_CONFIGURATION_WATCHER,
+  META_DATA_ROOT,
+  META_DATA_CRON,
   DEBUG_LOGS
 };
